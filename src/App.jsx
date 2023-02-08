15bd7c24424ce5f4027848e6598b4026cf92f848
Pedro Ramos <pedroaramosguardiola@gmail.com>
2023-02-08 19:26:00 +0100

    🐛 fix: relocate router const

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,28 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import { Topbar, AllComponent, ActiveComponent, CompletedComponent } from './components'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Topbar />,
-    children: [
-      {
-        path: "",
-        element: <AllComponent />,
-      },
-      {
-        path: "active",
-        element: <ActiveComponent />,
-      },
-      {
-        path: "completed",
-        element: <CompletedComponent />,
-      },
-    ],
-  }
-]);
-
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Topbar />,
+      children: [
+        {
+          path: "",
+          element: <AllComponent />,
+        },
+        {
+          path: "active",
+          element: <ActiveComponent />,
+        },
+        {
+          path: "completed",
+          element: <CompletedComponent />,
+        },
+      ],
+    }
+  ]);
 
   return (
     <>
