4e304a9583803ddbeedd6eefe2208907e2ddea34
Pedro Ramos <pedroaramosguardiola@gmail.com>
2023-02-08 19:04:45 +0100

    ✨ feat: navlink added to topbar to navigation

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,4 +1,5 @@
 ﻿import { Outlet } from "react-router"
+import { NavLink } from "react-router-dom"
 
 
 export const Topbar = () => {
@@ -7,20 +8,21 @@ export const Topbar = () => {
     <>
         <h1>#todo</h1>
         <div className="menu">
-          <a href="" className="menu-item active">
+          {/* React does not detect activeClassName on version 6, so this is used to add a active class to NavLink is used className={(navData) => (navData.isActive ? "active" : 'none')+ ' menu-item'}, in this case the class active is default so it is not needed*/}
+          <NavLink to={``} className="menu-item">
             All
             <div className="menu-item-border"></div>
-          </a>
+          </NavLink>
 
-          <a href="/active" className="menu-item">
+          <NavLink to={`active`} className="menu-item">
             Active
             <div className="menu-item-border"></div>
-          </a>
+          </NavLink>
 
-          <a href="/completed" className="menu-item">
+          <NavLink to={`completed`} className="menu-item">
             Completed
             <div className="menu-item-border"></div>
-          </a>
+          </NavLink>
         </div>
 
         <Outlet/>
