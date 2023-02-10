6aacc5be43f1c1debd6c4af891c7a9657ee021d4
Pedro Ramos <pedroaramosguardiola@gmail.com>
2023-02-10 23:48:38 +0100

    ⚡️💄 feat: dark theme colors & swith theme funct darktheme colors and updated theme switch functionality

--- src/hooks/theme.js
+++ src/hooks/theme.js
@@ -1,4 +1,4 @@
-﻿const root = document.querySelector(':root').style;
+﻿const root = document.querySelector(':root');
 
 export const getSystemTheme = () => {
   const theme = (localStorage.getItem('theme')) ? localStorage.getItem('theme') : (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
@@ -7,22 +7,6 @@ export const getSystemTheme = () => {
 }
 
 export const updateThemeMode = (theme) => {
-  let backgroundColor, mainTextColor;
-
-  switch(theme) {
-    case 'light': 
-      backgroundColor = '#fefefe';
-      mainTextColor = '#333333';
-      break;
-    
-    case 'dark':
-      backgroundColor = '#333333';
-      mainTextColor = '#fefefe';
-      break;
-  }
-  
-  root.setProperty('--background-color', backgroundColor);
-  root.setProperty('--main-text-color', mainTextColor);
-
+  root.className = theme;
   localStorage.setItem('theme', theme);
 }
