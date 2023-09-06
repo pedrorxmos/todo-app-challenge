c9614930051f24f64931de227d36ef90a0a4a5d9
Pedro Ramos <pedrorxmosdev@gmail.com>
2023-09-06 22:12:41 +0200

    🚀 deploy redirect

--- src/components/index.js
+++
@@ -1,2 +0,0 @@
-﻿export * from './Topbar';
-export * from './TasksComponent';
--- src/hooks/localStorage.js
+++
@@ -1,9 +0,0 @@
-﻿//Get tasks from localStorage
-export const getTasks = (key) => {
-	return localStorage.getItem(key) ? JSON.parse(localStorage.getItem(key)) : [];
-};
-
-//Update task to localStorage
-export const updateTasksStorage = (key, value) => {
-	localStorage.setItem(key, JSON.stringify(value));
-};
